93000fe859bdbc6d050f07749926fb55c32e1129
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-12-06 14:11:10 +0100

    Adds an example game field

--- MineSweeper.hs
+++ MineSweeper.hs
@@ -3,8 +3,20 @@ module MineSweeper where
 import DataTypes
 import RunGame
 
+example = GameField [
+    [ec, ec, ec, ec, ec], 
+    [e1, e1, e1, ec, ec],
+    [e1, bo, e2, e1, ec],
+    [e1, e2, bo, e1, ec],
+    [ec, e1, e1, e1, ec]]
+    where 
+        ec = Cell Closed (Numeric 0)
+        e1 = Cell Closed (Numeric 1)
+        e2 = Cell Closed (Numeric 2)
+        bo = Cell Closed Bomb
+
 newGame :: GameField
-newGame = (GameField [[]])
+newGame = example
 
 flagCell :: GameField -> Pos -> GameField
 flagCell _ _ = (GameField [[]])
