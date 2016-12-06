22ca6d3c0bf1bdfdb3b3013374ef08cb0ff668d8
Criticalstone <kevinhoog@gmail.com>
2016-12-06 14:34:11 +0100

    Adds function to print field

--- RunGame.hs
+++ RunGame.hs
@@ -1,6 +1,8 @@
 module RunGame where
 import DataTypes
 
+import Data.Char
+
 data Interface = Interface 
     {   iNewGame    :: GameField 
     ,   iFlagCell   :: GameField -> Pos -> GameField
@@ -8,3 +10,14 @@ data Interface = Interface
     ,   iHasWon     :: GameField -> Bool
     ,   iGameOver   :: GameField -> Bool
     }
+
+printField :: GameField -> IO ()
+printField (GameField rows) = 
+            putStrLn (unlines [ [ cellToChar c | c <- row ] | row <- rows] )
+
+cellToChar :: Cell -> Char
+cellToChar (Cell Closed _)              = '.'
+cellToChar (Cell Flagged _)             = 'P'
+cellToChar (Cell Opened (Numeric 0))    = ' ' 
+cellToChar (Cell _ (Numeric n))         = intToDigit n
+cellToChar _                            = '+'  
